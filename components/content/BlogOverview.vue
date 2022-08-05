975cbbb9bb2e128c4acb2a08a1c9ced046a52295
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-05 15:47:12 +0200

    wip: blog overview

--- components/content/BlogOverview.vue
+++ components/content/BlogOverview.vue
@@ -1,74 +1,30 @@
 <template>
-  <div
-    class="relative bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
-  >
-    <div class="absolute inset-0">
-      <div class="bg-white h-1/3 sm:h-2/3" />
-    </div>
+  <div class="relative">
     <div class="relative max-w-7xl mx-auto">
-      <div class="text-center">
-        <h2
-          class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl"
-        >
-          From the blog
-        </h2>
-        <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero
-          labore natus atque, ducimus sed.
-        </p>
-      </div>
-      <div
-        class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"
-      >
+      <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
         <div
           v-for="post in posts"
           :key="post.title"
-          class="flex flex-col rounded-lg shadow-lg overflow-hidden"
+          class="flex flex-col rounded-lg shadow-lg overflow-hidden dark:bg-primary-700"
         >
           <div class="flex-shrink-0">
             <img class="h-48 w-full object-cover" :src="post.imageUrl" alt="">
           </div>
-          <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+          <div class="flex-1 p-6 flex flex-col justify-between dark:bg-primary-700">
             <div class="flex-1">
-              <p class="text-sm font-medium text-indigo-600">
+              <!-- <p class="text-sm font-medium text-indigo-600">
                 <a :href="post.category.href" class="hover:underline">
                   {{ post.category.name }}
                 </a>
+              </p> -->
+              <!-- <a :href="post.href" class="block mt-2"> -->
+              <p class="text-xl font-semibold">
+                {{ post.title }}
               </p>
-              <a :href="post.href" class="block mt-2">
-                <p class="text-xl font-semibold text-gray-900">
-                  {{ post.title }}
-                </p>
-                <p class="mt-3 text-base text-gray-500">
-                  {{ post.description }}
-                </p>
-              </a>
-            </div>
-            <div class="mt-6 flex items-center">
-              <div class="flex-shrink-0">
-                <a :href="post.author.href">
-                  <span class="sr-only">{{ post.author.name }}</span>
-                  <img
-                    class="h-10 w-10 rounded-full"
-                    :src="post.author.imageUrl"
-                    alt=""
-                  >
-                </a>
-              </div>
-              <div class="ml-3">
-                <p class="text-sm font-medium text-gray-900">
-                  <a :href="post.author.href" class="hover:underline">
-                    {{ post.author.name }}
-                  </a>
-                </p>
-                <div class="flex space-x-1 text-sm text-gray-500">
-                  <time :datetime="post.datetime">
-                    {{ post.date }}
-                  </time>
-                  <span aria-hidden="true"> &middot; </span>
-                  <span> {{ post.readingTime }} read </span>
-                </div>
-              </div>
+              <p class="mt-3 text-base">
+                {{ post.description }}
+              </p>
+              <!-- </a> -->
             </div>
           </div>
         </div>
--- layouts/full-width.vue
+++ layouts/full-width.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-primary-800 sm:rounded">
-    <main class="px-4 mx-auto sm:px-8 prose-gray dark:prose-invert">
+    <main class="px-4 mx-auto sm:px-8 sm:mt-14 prose prose-gray text-center max-w-none dark:prose-invert">
       <slot>
         <h1 class="text-2xl">
           Page not found
