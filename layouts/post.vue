276e71e8d488d04f2fbe632bfa712866a9b7bf93
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-06 13:26:41 +0200

    chore: create new layout for blog post

---
+++ layouts/post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="py-10 m-auto sm:px-8 sm:shadow dark:bg-primary-800 sm:rounded">
+    <main class="px-4 mx-auto sm:px-8 sm:pt-14 prose prose-gray text-center text-primary dark:text-primary-100 max-w-none dark:prose-invert">
+      <slot>
+        <h1 class="text-2xl">
+          Blog post not found
+        </h1>
+      </slot>
+    </main>
+  </div>
+</template>
+
+<style lang="postcss">
+/* Customize headers to remove default underline */
+.prose h2 a,
+.prose h3 a {
+  @apply no-underline;
+  &:hover {
+    @apply border-b border-neutral-600 dark:border-neutral-300;
+  }
+}
+</style>
