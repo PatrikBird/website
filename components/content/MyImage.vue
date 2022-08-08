6853d03316cdfaa8ffd1773f8687eecefab430d5
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-08 13:55:46 +0200

    chore: make img component reusable
    and provide props from markdown

--- components/content/MyImage.vue
+++ components/content/MyImage.vue
@@ -1,14 +1,14 @@
-<!-- <script setup lang="ts">
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps<{pic: string, widepic: string, alt: string}>()
-</script> -->
+</script>
 
 <template>
-  <!-- <img src="test-resized.webp" alt="yummy food"> -->
   <picture>
     <source
-      srcset="/blog/first/test-resized-wide.webp"
+      :srcset="widepic"
       media="(min-width: 640px)"
     >
-    <img src="/blog/first/test-resized.webp" alt="yummy food" style="margin-top: 0">
+    <img :src="pic" :alt="alt" style="margin-top: 0">
   </picture>
 </template>
