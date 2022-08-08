b5a3afe8f714dccadfb2dcfb22fd014d405d7d82
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-08 13:34:13 +0200

    chore: wip image component

--- components/content/MyImage.vue
+++ components/content/MyImage.vue
@@ -2,9 +2,9 @@
   <!-- <img src="test-resized.webp" alt="yummy food"> -->
   <picture>
     <source
-      srcset="first/test-resized-wide.webp"
-      media="(min-width: 800px)"
+      srcset="/blog/first/test-resized-wide.webp"
+      media="(min-width: 640px)"
     >
-    <img src="first/test-resized.webp" alt="yummy food">
+    <img src="/blog/first/test-resized.webp" alt="yummy food">
   </picture>
 </template>
