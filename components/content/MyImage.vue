678fc0b4221b91f6c61ee70488b51536ca166ebe
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-08 13:06:04 +0200

    chore: wip picture component

---
+++ components/content/MyImage.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- <img src="test-resized.webp" alt="yummy food"> -->
+  <picture>
+    <source
+      srcset="first/test-resized-wide.webp"
+      media="(min-width: 800px)"
+    >
+    <img src="first/test-resized.webp" alt="yummy food" width="750" height="1125">
+  </picture>
+</template>
