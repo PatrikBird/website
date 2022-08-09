946551b062fa8895ddebd9b13f8f60c253d27faa
Patrik Vogel <patrik.vogel@paessler.com>
2022-08-09 15:19:34 +0200

    feat(blog): accept image captions via slot

--- components/content/PostImage.vue
+++ components/content/PostImage.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const props = defineProps<{pic: string, alt: string}>()
+const props = defineProps<{ pic: string; alt: string }>()
 </script>
 
 <template>
-  <img :src="pic" :alt="alt" width="600" height="400">
+  <figure>
+    <img :src="pic" :alt="alt" width="600" height="400">
+    <figcaption>
+      <slot>
+        <p>no caption provided</p>
+      </slot>
+    </figcaption>
+  </figure>
 </template>
