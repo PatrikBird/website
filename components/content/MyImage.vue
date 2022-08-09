afb25220b213078f27573d1da42e772ba2f3c104
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-09 07:40:40 +0200

    chore: play around with img component

--- components/content/MyImage.vue
+++ components/content/MyImage.vue
@@ -4,11 +4,15 @@ const props = defineProps<{pic: string, widepic: string, alt: string}>()
 </script>
 
 <template>
-  <picture>
-    <source
-      :srcset="widepic"
-      media="(min-width: 640px)"
-    >
-    <img :src="pic" :alt="alt" class="m-auto">
-  </picture>
+  <div>
+    <picture>
+      <source
+        srcset="/blog/first/test-resized-wide.webp 1500w"
+        sizes="(min-width: 1300px) 80vw"
+        media="(min-width: 640px)"
+        :alt="alt"
+      >
+      <img :src="pic" :alt="alt" class="m-auto">
+    </picture>
+  </div>
 </template>
