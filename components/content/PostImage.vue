3abe950ec153ea5ba43a4108469eb9595817d6bc
Patrik Vogel <patrik.vogel@paessler.com>
2022-08-09 15:13:00 +0200

    chore: create separate component for blog post images

---
+++ components/content/PostImage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<{pic: string, alt: string}>()
+</script>
+
+<template>
+  <img :src="pic" :alt="alt" width="600" height="400">
+</template>
