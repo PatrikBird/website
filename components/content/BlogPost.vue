33dbe19f520f10a463fd47918724d5bd581f89b5
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-09 08:04:52 +0200

    ui: tweak blog post layout

--- components/content/BlogPost.vue
+++ components/content/BlogPost.vue
@@ -1,18 +1,21 @@
 <template>
   <article>
+    <!-- Featured Img -->
     <div class="overflow-hidden">
       <slot name="featpic">
         <p>no pic found</p>
       </slot>
     </div>
+    <!-- Horizontal Line -->
     <div class="relative mb-8">
       <div class="absolute inset-0 flex items-center" aria-hidden="true">
         <div class="w-full border-t border-gray-300" />
       </div>
     </div>
-    <div class="pb-16 px-4 sm:pt-6 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto">
+    <!-- Content -->
+    <div class="pb-16 px-4 sm:pt-6 sm:px-6">
       <div class="lg:pl-8">
-        <div class="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto">
+        <div class="text-base max-w-prose mx-auto">
           <slot name="content">
             <p>no content provided</p>
           </slot>
