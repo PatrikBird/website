ecda15df8ce47506529ce25a74bbe521f39c62a0
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-10 08:47:11 +0200

    ui(blog): remove underlines in overview

--- pages/blog.vue
+++ pages/blog.vue
@@ -11,16 +11,17 @@ definePageMeta({
     </h2>
     <p class="mt-3 max-w-2xl mx-auto text-xl sm:mt-4">
       Lorem ipsum dolor sit amet consectetur, adipisicing
-      elit. Ipsa libero labore natus atque, ducimus sed.
+      elit. Ipsa libero labore natus      <a href="http://google.de">Google.de</a>
+      atque, ducimus sed.
     </p>
     <div class="max-w-7xl mx-auto">
-      <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      <div class="mt-12 max-w-lg mx-auto grid gap-4 lg:grid-cols-3 lg:max-w-none">
         <ContentList v-slot="{ list }" path="/blog" :query="{ sort: { date: -1 } }">
           <NuxtLink
             v-for="article in list"
             :key="article._path"
             :to="article._path"
-            class="flex flex-col rounded-lg shadow-lg overflow-hidden dark:bg-primary-900 duration-100 ease-in-out transform hover:scale-105"
+            class="flex flex-col no-underline rounded-lg shadow-lg overflow-hidden dark:bg-primary-900 duration-100 ease-in-out transform hover:scale-105"
           >
             <div class="flex-shrink-0">
               <img class="h-48 w-full object-cover" :src="article.imageUrl" alt="">
