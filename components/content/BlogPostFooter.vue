5844d89f583eefea58215d6bc0d835e95a787f94
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-08-30 10:28:44 +0200

    ui: align blog footer in rows on small screens

--- components/content/BlogPostFooter.vue
+++ components/content/BlogPostFooter.vue
@@ -4,8 +4,8 @@ const { prev, next } = useContent()
 </script>
 
 <template>
-  <div class="flex justify-between">
-    <NuxtLink v-if="prev && prev._path.includes('blog')" :to="prev._path" class="opacity-75 hover:opacity-100">
+  <div class="flex flex-col justify-center sm:flex-row sm:justify-between gap-3">
+    <NuxtLink v-if="prev && prev._path.includes('blog')" :to="prev._path" class="m-auto sm:m-0 truncate opacity-75 hover:opacity-100">
       <Icon name="line-md:arrow-left-circle" class="opacity-75 hover:opacity-100" />
       {{ prev.title }}
     </NuxtLink>
@@ -13,12 +13,13 @@ const { prev, next } = useContent()
       v-else
       :to="route.path.split('/').slice(0, -1).join('/') || '/'"
       title="Zurück zum Blog"
-      class="opacity-75 hover:opacity-100"
+      class="m-auto sm:m-0 opacity-75 hover:opacity-100"
     >
+      <Icon name="line-md:arrow-left-circle" class="opacity-75 hover:opacity-100" />
       Zum Blog
     </NuxtLink>
 
-    <NuxtLink v-if="next" :to="next._path" class="opacity-75 hover:opacity-100">
+    <NuxtLink v-if="next" :to="next._path" class="m-auto sm:m-0 truncate opacity-75 hover:opacity-100">
       {{ next.title }}
       <Icon name="line-md:arrow-right-circle" class="opacity-75 hover:opacity-100" />
     </NuxtLink>
@@ -26,9 +27,10 @@ const { prev, next } = useContent()
       v-else
       :to="route.path.split('/').slice(0, -1).join('/') || '/'"
       title="Zurück zum Blog"
-      class="opacity-75 hover:opacity-100"
+      class="m-auto sm:m-0 opacity-75 hover:opacity-100"
     >
       Zum Blog
+      <Icon name="line-md:arrow-right-circle" class="opacity-75 hover:opacity-100" />
     </NuxtLink>
   </div>
 </template>
--- components/content/ScrollToTop.vue
+++ components/content/ScrollToTop.vue
@@ -11,7 +11,12 @@ function scrollToTop() {
 
 <template>
   <div>
-    <button type="button" :class="{ hidden: isHidden }" class="fixed right-5 bottom-5 p-1 rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700" @click="scrollToTop">
+    <button
+      type="button"
+      :class="{ hidden: isHidden }"
+      class="fixed right-4 md:right-12 bottom-4 md:bottom-12 p-1 rounded-full shadow-sm text-white bg-accent hover:bg-accent-800"
+      @click="scrollToTop"
+    >
       <Icon name="material-symbols:arrow-circle-up-outline-rounded" class="h-7 w-7" aria-hidden="true" />
     </button>
   </div>
