3080748c4f34b9ee62aa40fa28259d799e6f5bde
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-09-21 14:10:53 +0200

    ui(blog): adjust blog post look

--- pages/blog.vue
+++ pages/blog.vue
@@ -14,7 +14,8 @@ definePageMeta({
       Blog
     </h2>
     <p class="mt-3 max-w-2xl mx-auto text-xl sm:mt-4">
-      For now, the blog will be exclusively about my upcoming trip to inform friends and family and is therefore written in German.
+      For now, the blog will be exclusively about my upcoming trip to inform friends and family and is therefore written
+      in German.
     </p>
     <div class="max-w-7xl mx-auto">
       <div class="mt-12 max-w-lg mx-auto grid gap-4 lg:grid-cols-3 lg:max-w-none">
@@ -23,14 +24,17 @@ definePageMeta({
             v-for="article in list"
             :key="article._path"
             :to="article._path"
-            class="flex flex-col no-underline rounded-lg shadow-lg hover:shadow-xl border dark:border-primary-700 overflow-hidden dark:bg-primary-700 transition-all duration-200 ease-in-out"
+            class="flex flex-col no-underline rounded-lg shadow hover:shadow-xl bg-zinc-50 dark:bg-zinc-800/50 overflow-hidden hover:-translate-y-1 dark:bg-primary-700 transition-transform transition-shadow duration-200 ease-in-out"
           >
             <div class="flex-shrink-0">
               <img class="h-48 w-full object-cover" :src="article.imageUrl" width="630" height="290" alt="">
             </div>
+            <date class="text-xs italic">
+              {{ article.date }}
+            </date>
             <div class="flex-1 px-6 pb-6 flex flex-col justify-between dark:bg-primary-700">
               <div class="flex-1">
-                <p class="text-xl font-semibold">
+                <p class="text-xl mt-0">
                   {{ article.title }}
                 </p>
                 <p class="mt-3 text-base">
