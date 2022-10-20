be27626c10266c5db8a797196aec35675fd3a21f
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-10-20 20:49:46 +0700

    chore: write blogpost

--- components/content/GoogleMaps.vue
+++ components/content/GoogleMaps.vue
@@ -1,26 +1,56 @@
 <script setup lang='ts'>
 
 const props = defineProps<{ lat: number, lng: number }>()
-
 const center = ref({ lat: props.lat, lng: props.lng })
-const marker = {
-  id: 'nbg',
-  position: {
-    lat: props.lat, lng: props.lng
-  }
+
+const openedMarkerID = ref(null)
+function openMarker(id) {
+  openedMarkerID.value = id
 }
-// const markers = ref({
-//   markers: [
-//     {
-//       description: "Google France",
-//       id: "1",
-//       position: {
-//         lat: 48.8773406,
-//         lng: 2.327774,
-//       },
-//     },
-//   ]
-// });
+
+const markers = ref([
+  {
+    description: 'Bangkok, Thailand',
+    id: '1',
+    position: {
+      lat: 13.76,
+      lng: 100.5
+    }
+  },
+  {
+    description: 'Ayutthaya - the Ancient Capital of Thailand, and is one of the most visited historical sites in the country.',
+    id: '2',
+    position: {
+      lat: 14.37,
+      lng: 100.59
+    }
+  },
+  {
+    description: 'Phitsanulok',
+    id: '3',
+    position: {
+      lat: 16.82,
+      lng: 100.26
+    }
+  },
+  {
+    description: 'Sukhothai new and old city (Historical Park) and Ramkhamhaeng National Park',
+    id: '4',
+    position: {
+      lat: 17,
+      lng: 99.83
+    }
+  },
+  {
+    description: 'Lampang',
+    id: '5',
+    position: {
+      lat: 18.29,
+      lng: 99.49
+    }
+  }
+]
+)
 </script>
 
 <template>
@@ -36,19 +66,24 @@ const marker = {
         rotateControl: false,
         fullscreenControl: false,
         backgroundColor: '#f1f1f1',
-        zoom: 7,
+        zoom: 6,
         minZoom: 4,
         mapTypeId: 'terrain',
       }"
       style="width: 100%; height: 600px; margin: auto"
     >
-      <GMapMarker :key="marker.id" :position="marker.position" />
-      <!-- <GMapMarker :key="index" v-for="(marker, index) in markers" :position="marker.position" :clickable="true"
-        :draggable="true" @click="openMarker(marker.id)">
-        <GMapInfoWindow :closeclick="true" @closeclick="openMarker(null)" :opened="openedMarkerID === marker.id">
+      <GMapMarker v-for="marker in markers" :key="marker.id" :position="marker.position" />
+      <GMapMarker
+        v-for="(marker, index) in markers"
+        :key="index"
+        :position="marker.position"
+        :clickable="true"
+        @click="openMarker(marker.id)"
+      >
+        <GMapInfoWindow :closeclick="true" :opened="openedMarkerID === marker.id" @closeclick="openMarker(null)">
           <div>{{ marker.description }}</div>
         </GMapInfoWindow>
-      </GMapMarker> -->
+      </GMapMarker>
     </GMapMap>
   </div>
 </template>
