dc5af75a6f265fd24deb17e995132d5e52402ef6
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-11 21:21:17 +0700

    chore: add image resize script

---
+++ utils/webp-convert-directory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PARAMS=('-m 6 -q 90 -resize 1000 0 -mt -progress')
+
+if [ $# -ne 0 ]; then
+	PARAMS=$@;
+fi
+
+cd $(pwd)
+
+shopt -s nullglob nocaseglob extglob
+
+for FILE in *.@(jpg|jpeg|tif|tiff|png); do 
+    cwebp $PARAMS "$FILE" -o "${FILE%.*}".webp;
+done
+
