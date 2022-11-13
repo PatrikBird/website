569c79af8a6a6562e604412303401fbedc3e2779
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-13 17:12:32 +0700

    chore: fix socials

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
     size: '22px' // default <Icon> size applied
   },
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind'
+    twitter: 'PatBirdMe',
+    github: 'PatrikBird'
   }
 })
