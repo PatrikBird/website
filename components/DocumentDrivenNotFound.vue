2597e1fbcc263fd99fb5dc3e17cfaf3ef03cc0ba
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-21 20:44:29 +0700

    chore: fix 404 page dark mode style

--- components/DocumentDrivenNotFound.vue
+++ components/DocumentDrivenNotFound.vue
@@ -8,18 +8,18 @@
     <body class="h-full">
     ```
   -->
-  <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+  <div class="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
     <div class="max-w-max mx-auto">
       <main class="sm:flex">
-        <p class="text-4xl tracking-tight font-bold text-indigo-600 sm:text-5xl">
+        <p class="text-4xl tracking-tight font-bold sm:text-5xl">
           404
         </p>
         <div class="sm:ml-6">
           <div class="sm:border-l sm:border-gray-200 sm:pl-6">
-            <h1 class="text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl">
+            <h1 class="text-4xl font-bold tracking-tight sm:text-5xl">
               Page not found
             </h1>
-            <p class="mt-1 text-base text-gray-500">
+            <p class="mt-1 text-base">
               Please check the URL in the address bar and try again.
             </p>
           </div>
