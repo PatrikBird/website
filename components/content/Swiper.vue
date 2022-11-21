80da1360cd488edc9837518a21259dcc1ec5ea3e
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-21 20:12:33 +0700

    chore: add swiper

---
+++ components/content/Swiper.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+
+const onSwiper = (swiper) => {
+  console.log(swiper)
+}
+const onSlideChange = () => {
+  console.log('slide change')
+}
+</script>
+
+<template>
+  <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>
+      <img src="https://picsum.photos/200/300" alt="random">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://picsum.photos/200/300" alt="random">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://picsum.photos/200/300" alt="random">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://picsum.photos/200/300" alt="random">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://picsum.photos/200/300" alt="random">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://picsum.photos/200/300" alt="random">
+    </swiper-slide>
+  </swiper>
+</template>
