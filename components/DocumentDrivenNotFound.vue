e63776dcbb8f68f90a308e4fe0c6f532bd95aafa
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-24 19:01:20 +0700

    chore: delete ded code

--- components/DocumentDrivenNotFound.vue
+++ components/DocumentDrivenNotFound.vue
@@ -1,13 +1,4 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full">
-    <body class="h-full">
-    ```
-  -->
   <div class="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
     <div class="max-w-max mx-auto">
       <main class="sm:flex">
