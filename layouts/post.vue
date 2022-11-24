ebc69c16c9f19e76b35806d25e3811ecb1bf808b
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-24 19:49:16 +0700

    feat: add back button to blog post

--- layouts/post.vue
+++ layouts/post.vue
@@ -1,9 +1,19 @@
+<script setup lang='ts'>
+const route = useRoute()
+</script>
+
 <template>
   <div>
     <ReadProgressBar />
     <ScrollToTop />
-    <div class="max-w-3xl px-4 py-10 m-auto sm:px-8 sm:pt-14 dark:bg-primary-800 sm:rounded">
+    <div class="max-w-3xl px-4 py-10 m-auto sm:px-8 sm:pt-14 dark:bg-primary-800 sm:rounded lg:relative">
       <div class="prose prose-gray dark:prose-invert max-w-none">
+        <NuxtLink
+          class="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:top-15 lg:-left-10"
+          :to="route.path.split('/').slice(0, -1).join('/') || '/'"
+        >
+          <Icon name="material-symbols:arrow-circle-left-outline-rounded" class="h-7 w-7" aria-hidden="true" />
+        </NuxtLink>
         <article>
           <slot />
         </article>
