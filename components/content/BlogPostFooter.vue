7ae7a88f957805744222d74f85355ff204c88792
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-24 21:22:30 +0700

    ui: clean up blog navigation

--- components/content/BlogPostFooter.vue
+++ components/content/BlogPostFooter.vue
@@ -1,5 +1,4 @@
 <script setup lang='ts'>
-// const route = useRoute()
 const { prev, next } = useContent()
 </script>
 
@@ -14,36 +13,17 @@ const { prev, next } = useContent()
       <NuxtLink
         v-if="prev && prev._path.includes('blog')"
         :to="prev._path"
-        class="m-auto sm:m-0 truncate opacity-75 hover:opacity-100"
+        class="m-auto sm:m-0 truncate no-underline opacity-75 hover:opacity-100"
       >
-        <Icon name="line-md:arrow-left-circle" class="opacity-75 hover:opacity-100" />
+        <Icon name="line-md:arrow-small-left" class="opacity-75 hover:opacity-100" />
         {{ prev.title }}
       </NuxtLink>
-      <!-- <NuxtLink
-      v-else
-      :to="route.path.split('/').slice(0, -1).join('/') || '/'"
-      title="Zurück zum Blog"
-      class="m-auto sm:m-0 opacity-75 hover:opacity-100"
-      >
-      <Icon name="line-md:arrow-left-circle" class="opacity-75 hover:opacity-100" />
-      Zum Blog
-    </NuxtLink> -->
       <div v-else />
 
-      <NuxtLink v-if="next" :to="next._path" class="m-auto sm:m-0 truncate opacity-75 hover:opacity-100">
+      <NuxtLink v-if="next" :to="next._path" class="m-auto sm:m-0 truncate no-underline opacity-75 hover:opacity-100">
         {{ next.title }}
-        <Icon name="line-md:arrow-right-circle" class="opacity-75 hover:opacity-100" />
+        <Icon name="line-md:arrow-small-left" class="rotate-180 opacity-75 hover:opacity-100" />
       </NuxtLink>
-      <!-- <NuxtLink
-      v-else
-      disabled
-      :to="route.path.split('/').slice(0, -1).join('/') || '/'"
-      title="Zurück zum Blog"
-      class="m-auto sm:m-0 opacity-75 hover:opacity-100"
-      >
-      Zum Blog
-      <Icon name="line-md:arrow-right-circle" class="opacity-75 hover:opacity-100" />
-    </NuxtLink> -->
       <div v-else />
     </div>
   </footer>
