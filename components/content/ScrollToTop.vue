f6d4acffe13a70b4075586ed294c659f786b90ca
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-24 20:41:05 +0700

    ui: use sweet button design

--- components/content/ScrollToTop.vue
+++ components/content/ScrollToTop.vue
@@ -14,10 +14,10 @@ function scrollToTop() {
     <button
       type="button"
       :class="{ hidden: isHidden }"
-      class="fixed right-4 sm:right-12 bottom-4 sm:bottom-12 p-1 rounded-full shadow-sm text-white bg-secondary-500 hover:bg-secondary-700"
+      class="fixed right-4 sm:right-12 bottom-4 sm:bottom-12 group h-10 w-10 p-1 rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
       @click="scrollToTop"
     >
-      <Icon name="material-symbols:arrow-circle-up-outline-rounded" class="h-7 w-7" aria-hidden="true" />
+      <Icon name="line-md:arrow-small-left" class="rotate-90 opacity-75 group-hover:opacity-100 text-secondary-800 dark:text-primary-100" aria-hidden="true" />
     </button>
   </div>
 </template>
--- layouts/post.vue
+++ layouts/post.vue
@@ -9,10 +9,10 @@ const route = useRoute()
     <div class="max-w-3xl px-4 py-10 m-auto sm:px-8 sm:pt-14 dark:bg-primary-800 sm:rounded lg:relative">
       <div class="prose prose-gray dark:prose-invert max-w-none">
         <NuxtLink
-          class="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:top-15 lg:-left-10"
+          class="group mb-8 flex h-10 w-10 lg:absolute lg:top-15 lg:-left-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
           :to="route.path.split('/').slice(0, -1).join('/') || '/'"
         >
-          <Icon name="material-symbols:arrow-circle-left-outline-rounded" class="h-7 w-7" aria-hidden="true" />
+          <Icon name="line-md:arrow-small-left" class="opacity-75 group-hover:opacity-100 text-secondary-800 dark:text-primary-100" aria-hidden="true" />
         </NuxtLink>
         <article>
           <slot />
