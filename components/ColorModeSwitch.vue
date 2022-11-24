e9a8b92befd09cb503d847f1e3f2ff44b039b0ab
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-24 21:28:35 +0700

    ui: fix transition of buttons

--- components/ColorModeSwitch.vue
+++ components/ColorModeSwitch.vue
@@ -4,7 +4,7 @@ const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'da
 </script>
 
 <template>
-  <button aria-label="Color Mode" class="inline-block group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20" @click="onClick">
+  <button aria-label="Color Mode" class="inline-block group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20" @click="onClick">
     <ColorScheme placeholder="...">
       <Icon v-if="colorMode.value === 'dark'" name="line-md:moon" />
       <Icon v-else name="line-md:sunny-outline-loop" />
