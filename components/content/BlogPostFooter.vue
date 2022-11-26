105502b9c2e507ea3269711442e83414825660c9
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-26 15:30:14 +0700

    chore: dont render footer if only one post

--- components/content/BlogPostFooter.vue
+++ components/content/BlogPostFooter.vue
@@ -3,7 +3,7 @@ const { prev, next } = useContent()
 </script>
 
 <template>
-  <footer>
+  <footer v-if="prev && prev._path.includes('blog') || next && next._path.includes('blog')">
     <div class="relative mb-8">
       <div class="absolute inset-0 flex items-center" aria-hidden="true">
         <div class="w-full border-t border-gray-300" />
