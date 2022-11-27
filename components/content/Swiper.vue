295aa7fe91f745c3bf93ae458c30bb0b3479b523
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-11-27 21:12:20 +0700

    chore: add pagination to swiper component

--- components/content/Swiper.vue
+++ components/content/Swiper.vue
@@ -1,20 +1,39 @@
 <script lang="ts" setup>
+// import Swiper core and required modules
+import { Pagination, Lazy } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
+
 import 'swiper/css'
+import 'swiper/css/pagination'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps<{
-  picUrls: string[]
+  pics: string[][] // [src, alt]
 }>()
+
+const images = computed(() => {
+  return props.pics.map((pic) => {
+    return {
+      src: pic[0],
+      alt: pic[1]
+    }
+  })
+})
+
+const modules = [Pagination, Lazy]
 </script>
 
 <template>
   <swiper
+    :modules="modules"
     :slides-per-view="1"
     :space-between="10"
+    :pagination="{ clickable: true }"
   >
-    <swiper-slide v-for="pic in picUrls" :key="pic">
-      <img :src="pic" alt="random">
+    <swiper-slide v-for="image in images" :key="image.src">
+      <post-image :pic="image.src" :alt="image.alt">
+        {{ image.alt }}
+      </post-image>
     </swiper-slide>
   </swiper>
 </template>
