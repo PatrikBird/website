9b59de2dbcdb1315fcda478715a5b06f13030c99
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-12-03 21:37:56 +0700

    chore: adds z-index to scroll to top button

--- components/content/ScrollToTop.vue
+++ components/content/ScrollToTop.vue
@@ -14,7 +14,7 @@ function scrollToTop() {
     <button
       type="button"
       :class="{ hidden: isHidden }"
-      class="fixed right-4 sm:right-12 bottom-4 sm:bottom-12 group h-10 w-10 p-1 rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+      class="fixed right-4 z-50 sm:right-12 bottom-4 sm:bottom-12 group h-10 w-10 p-1 rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
       title="Scroll to top"
       @click="scrollToTop"
     >
