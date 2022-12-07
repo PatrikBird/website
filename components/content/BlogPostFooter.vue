7f8ca64bb69fb29ea9460b418405e94b19c6ca35
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-12-07 22:33:43 +0700

    chore: fix border

--- components/content/BlogPostFooter.vue
+++ components/content/BlogPostFooter.vue
@@ -19,7 +19,7 @@ const hasTwoAdjacentBlogPosts = computed(() => {
         v-if="prev && prev._path.includes('blog')"
         :to="prev._path"
         class="no-underline flex flex-col border-b border-primary-500 py-6 text-center sm:border-0 opacity-75 hover:opacity-100"
-        :class="{'sm:border-r': hasTwoAdjacentBlogPosts}"
+        :class="{'sm:border-r': hasTwoAdjacentBlogPosts, 'border-b': !hasTwoAdjacentBlogPosts}"
       >
         <dd class="order-0 leading-6 text-sm">
           {{ prev.date }}
