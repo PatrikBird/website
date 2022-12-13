2f203fb87a1ef490df3d125f4567a4d9c1d89572
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2022-12-13 20:54:48 +0700

    refactor: use custom class

--- layouts/post.vue
+++ layouts/post.vue
@@ -9,7 +9,7 @@ const route = useRoute()
     <div class="max-w-3xl px-4 py-10 m-auto sm:px-8 sm:pt-14 dark:bg-primary-800 sm:rounded lg:relative">
       <div class="prose prose-gray dark:prose-invert max-w-none">
         <NuxtLink
-          class="group mb-8 flex h-10 w-10 lg:absolute lg:top-15 lg:-left-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+          class="action-btn mb-8 w-10 h-10 flex group lg:absolute lg:top-15 lg:-left-10"
           :to="route.path.split('/').slice(0, -1).join('/') || '/'"
           title="Back to blog"
         >
