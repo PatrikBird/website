7d8f580af3bc6f10c06538d44066cf8b93f81411
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-14 15:43:43 +0900

    chore: cleanup

--- layouts/fullwidth.vue
+++ layouts/fullwidth.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-auto py-10 sm:rounded sm:px-8">
-    <main class="prose mx-auto max-w-none px-4 text-center dark:prose-invert sm:px-8 sm:pt-14">
+    <main class="prose mx-auto max-w-none text-center dark:prose-invert sm:px-4 sm:pt-14">
       <slot>
         <h1 class="text-2xl">
           Page not found
@@ -9,14 +9,3 @@
     </main>
   </div>
 </template>
-
-<style lang="postcss">
-/* Customize headers to remove default underline */
-.prose h2 a,
-.prose h3 a {
-  @apply no-underline;
-  &:hover {
-    @apply border-b border-neutral-600 dark:border-neutral-300;
-  }
-}
-</style>
--- layouts/post.vue
+++ layouts/post.vue
@@ -24,15 +24,3 @@ const route = useRoute()
     </div>
   </div>
 </template>
-
-<!-- <style lang="postcss">
-/* Customize headers to remove default underline */
-.prose h2 a,
-.prose h3 a {
-  @apply no-underline;
-
-  &:hover {
-    @apply border-b border-neutral-600 dark:border-neutral-300;
-  }
-}
-</style> -->
