4bccd649473061f9c1b418f068d5805b566c1649
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-16 11:39:07 +0900

    chore: simplify layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="m-auto max-w-2xl px-4 py-10 sm:rounded sm:px-8 sm:pt-14">
-    <main class="prose prose-gray max-w-none dark:prose-invert">
-      <slot />
-    </main>
-  </div>
+  <main class="prose m-auto max-w-2xl px-4 py-10 dark:prose-invert sm:rounded sm:px-8 sm:pt-14">
+    <slot />
+  </main>
 </template>
