fdbf37ea45ccb3a606be3b116559c851f9dcb022
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-16 09:28:26 +0900

    chore: add padding

--- layouts/fullwidth.vue
+++ layouts/fullwidth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-auto py-10 sm:rounded sm:px-8">
+  <div class="m-auto py-10 px-4 sm:rounded sm:px-8">
     <main class="prose mx-auto max-w-none text-center dark:prose-invert sm:px-4 sm:pt-14">
       <slot>
         <h1 class="text-2xl">
