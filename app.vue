c3030811d6c7655a30b896654097ddeb85c4beca
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-03-06 09:12:30 +0900

    chore: delete dead code

--- app.vue
+++ app.vue
@@ -3,15 +3,3 @@
     <NuxtPage />
   </AppLayout>
 </template>
-
-<!-- <style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-</style> -->
