c5fa6feaa6055ea588c2935277824b3d47e4d044
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-09-05 07:47:09 +0700

    fix: cross browser compatibility for date

--- composables/date.ts
+++ composables/date.ts
@@ -1,7 +1,25 @@
-export function formatDate(date: Date) {
-  return new Date(date).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  })
+export function formatDate(input: Date | string): string {
+  let date: Date
+
+  if (typeof input === 'string') {
+    const [year, month, day] = input.split('.').map(Number)
+    date = new Date(year, month - 1, day) // Months are 0-based in JavaScript
+  }
+  else if (input instanceof Date) {
+    date = input
+  }
+  else {
+    throw new TypeError('Invalid date format')
+  }
+
+  const monthsShort = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+  ]
+
+  const day = date.getDate()
+  const month = monthsShort[date.getMonth()]
+  const year = date.getFullYear()
+
+  return `${day < 10 ? `0${day}` : day} ${month} ${year}`
 }
