8d3281875e5b4bdbda1d001ac91283607a887dc2
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-09-09 16:53:53 +0700

    chore: finish removing contentdoc

---
+++ pages/articles/index.vue
@@ -0,0 +1,57 @@
+<script setup lang='ts'>
+import type { Post } from '~/types'
+
+useHead({
+  title: 'Articles - Patrik Bird',
+})
+
+definePageMeta({
+  layout: 'fullwidth',
+})
+
+const postsLatestQuery = useLazyAsyncData(
+  'articles-latest-data',
+  () => queryContent<Post>('articles')
+    // Selects only two kinds of paths:
+    //   /travel/**
+    //   /travel/**/index
+    .where({ _path: /^(?:\/[^\/]+){2}$/ })
+    .sort({ date: -1 })
+    .find(),
+)
+
+const allArticles = computed(() => {
+  return postsLatestQuery.data.value
+})
+</script>
+
+<template>
+  <div>
+    <h1>Articles</h1>
+    <p class="mx-auto mt-3 max-w-2xl text-xl sm:mt-4">
+      Bla intro technical articles here.
+    </p>
+    <div class="mx-auto max-w-7xl">
+      <div
+        class="mx-auto mt-12 grid gap-6 px-6"
+      >
+        <article
+          v-for="article in allArticles"
+          :key="article._path"
+        >
+          <NuxtLink
+            :to="article._path"
+            class="relative flex min-h-[40px] flex-col items-start gap-3 border no-underline opacity-75 transition-opacity duration-200 hover:opacity-100 md:flex-row"
+          >
+            <div class="md:self-center">
+              {{ article.title }}
+            </div>
+            <div class="text-xs md:self-center">
+              {{ formatDate(article.date) }} &times; {{ article.readingTime.text }}
+            </div>
+          </NuxtLink>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/travel/[...travel].vue
@@ -0,0 +1,40 @@
+<script setup lang='ts'>
+import type { Post } from '~/types'
+
+definePageMeta({
+  layout: false,
+})
+
+const currentPath = useRoute().path
+const currentPost = await useAsyncData(`post-[${currentPath}]`, () => queryContent<Post>(currentPath).findOne())
+
+const currentError = currentPost.error.value
+if (currentError)
+  throw currentError
+
+const currentParsedContent = currentPost.data.value
+if (!currentParsedContent) {
+  throw createError({
+    statusCode: 404,
+    message: `Post not found at location '${currentPath}'`,
+  })
+}
+</script>
+
+<template>
+  <main class="prose prose-zinc m-auto max-w-3xl px-4 py-10 dark:prose-invert sm:rounded sm:px-8 sm:pt-14 lg:relative">
+    <Html lang="de" />
+    <ReadProgressBar />
+    <ScrollToTop />
+    <GoUpOnePageBtn />
+    <header v-if="currentParsedContent">
+      <h1 class="text-4xl">
+        {{ currentParsedContent.title }}
+      </h1>
+      <p>{{ formatDate(currentParsedContent.date) }} · {{ currentParsedContent.readingTime.text }}</p>
+    </header>
+    <article>
+      <ContentRendererMarkdown :value="currentParsedContent" tag="div" />
+    </article>
+  </main>
+</template>
