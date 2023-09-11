4abea5cf8535424f9853e711b927fd725c08c094
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-09-11 17:27:25 +0700

    fix: navbar style
    remove border

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -13,17 +13,13 @@
           <div class="flex items-center space-x-5 text-sm">
             <NuxtLink
               to="/"
-              active-class="!b-black"
-              class="no-underline opacity-75 hover:opacity-100 p-1"
-              border="b-2 b-solid b-white hover:b-black/50"
+              class="no-underline opacity-75 hover:opacity-100 p-1 text-black dark:text-white"
             >
               Home
             </NuxtLink>
             <NuxtLink
               to="/travel"
-              active-class="!b-black"
-              class="no-underline opacity-75 hover:opacity-100 p-1"
-              border="b-2 b-solid b-white hover:b-black/50"
+              class="no-underline opacity-75 hover:opacity-100 p-1 text-black dark:text-white"
             >
               Travel
             </NuxtLink>
@@ -31,7 +27,6 @@
               to="/articles"
               active-class="!b-black"
               class="no-underline opacity-75 hover:opacity-100 p-1"
-              border="b-2 b-solid b-white hover:b-black/50"
             >
               Articles
             </NuxtLink> -->
@@ -39,13 +34,13 @@
               href="https://github.com/PatrikBird"
               target="_blank"
               title="GitHub"
-              class="opacity-75 hover:opacity-100 dark:text-zinc-100"
+              class="opacity-75 hover:opacity-100 dark:text-zinc-100 text-black"
               aria-label="Go to GitHub"
             >
               <div i-iconoir:github />
             </a>
             <ColorModeSwitch
-              class="opacity-75 hover:opacity-100 dark:text-zinc-100"
+              class="opacity-75 hover:opacity-100 text-black dark:text-zinc-100"
               title="Toggle color mode"
             />
           </div>
