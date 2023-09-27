c1949cc122cfef28e76ac384ce013e34bc57aced
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-09-27 10:38:37 +0900

    chore(deps): update

--- plugins/vueGoogleMaps.ts
+++ plugins/vueGoogleMaps.ts
@@ -1,6 +1,6 @@
 import VueGoogleMaps from '@fawmi/vue-google-maps'
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin((nuxtApp: any) => {
   const apiKey = useRuntimeConfig().public.googleMapsApiKey
   nuxtApp.vueApp.use(VueGoogleMaps, {
     load: {
