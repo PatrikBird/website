f26d5af2d19f68112bbe7e2dabda94319677fd0c
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-10-07 16:44:49 +0900

    fix: animated img bad request

---
+++ components/content/LocalPic.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  src: string
+  alt: string
+  loading?: 'eager' | 'lazy'
+}>(), {
+  loading: 'lazy',
+})
+</script>
+
+<template>
+  <figure>
+    <img
+      :src="props.src"
+      :alt="props.alt"
+      :loading="props.loading"
+      class="w-full"
+      width="600"
+    >
+    <figcaption v-if="alt">
+      {{ alt }}
+    </figcaption>
+  </figure>
+</template>
