2d375756545c40e24435ac0f4848339a15f21275
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-03 12:08:05 +0900

    chore: remove comment

--- app.vue
+++ app.vue
@@ -2,7 +2,6 @@
   <div class="app font-sans">
     <Html lang="en" />
     <Meta property="og:image" content="/cover.jpg" />
-    <!-- <NuxtLoadingIndicator color="#FF0000" /> -->
     <AppLoadingBar />
     <AppNavbar />
     <NuxtLayout>
