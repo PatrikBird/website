dd57d1dcdafc197aeb6905dafdb62f991ec1b93d
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-27 21:00:35 +0700

    feat: custom list workaround

---
+++ components/content/CustomList.vue
@@ -0,0 +1,14 @@
+<script setup lang='ts'>
+const props = defineProps<{
+  items: string[]
+  icon: string
+}>()
+</script>
+
+<template>
+  <ul class="list-none">
+    <li v-for="item in props.items" :key="item" class="flex items-center gap2">
+      <span i-carbon:information-filled class="w-5 h-5" /> {{ item }}
+    </li>
+  </ul>
+</template>
