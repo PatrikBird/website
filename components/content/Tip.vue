828cd0a3536aa882e71207031b8b5cf5d2ddf154
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-27 16:20:12 +0700

    chore: tip component

--- components/content/Tip.vue
+++ components/content/Tip.vue
@@ -6,19 +6,19 @@ const props = withDefaults(defineProps<{ icon?: string }>(), {
 
 <template>
   <div class="p-4 rounded-md bg-blue-50 dark:bg-blue-900/35">
-    <div class="flex gap3">
+    <div class="flex gap3 items-center">
       <div class="flex-shrink-0">
         <span
           :class="props.icon"
-          class="w-5 h-5 pt2"
+          class="w-5 h-5"
           aria-hidden="true"
         />
       </div>
       <div>
-        <p class="m-0 font-600 text-lg">
+        <p class="m-0 text-lg">
           <ContentSlot :use="$slots.title" unwrap="p" />
         </p>
-        <div class="mt-2 text-sm">
+        <div class="text-sm">
           <ContentSlot :use="$slots.default" unwrap="p" />
         </div>
       </div>
