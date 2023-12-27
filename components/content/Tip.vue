1458d309145c504d1026d00295e6d0bc1c72a878
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-27 14:27:33 +0700

    chore: continue blog

---
+++ components/content/Tip.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ icon?: string }>(), {
+  icon: 'i-carbon:information',
+})
+</script>
+
+<template>
+  <div class="p-4 rounded-md bg-blue-50 dark:bg-blue-900/35">
+    <div class="flex gap3">
+      <div class="flex-shrink-0">
+        <span
+          :class="props.icon"
+          class="w-5 h-5 pt2"
+          aria-hidden="true"
+        />
+      </div>
+      <div>
+        <p class="m-0 font-600 text-lg">
+          <ContentSlot :use="$slots.title" unwrap="p" />
+        </p>
+        <div class="mt-2 text-sm">
+          <ContentSlot :use="$slots.default" unwrap="p" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
