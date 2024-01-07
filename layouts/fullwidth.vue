26d76350c97b4660ac5e741adc3942552dd14869
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2024-01-07 11:05:27 +0700

    chore: use unocss transforms

--- layouts/fullwidth.vue
+++ layouts/fullwidth.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="fullwidth-layout prose prose-zinc m-auto max-w-none px-0 py-10 text-center dark:prose-invert sm:px-8 sm:pt-14">
+  <div
+    class="fullwidth-layout prose prose-zinc m-auto max-w-none px-0 py-10 text-center
+    dark:prose-invert sm:px-8 sm:pt-14"
+  >
     <slot />
   </div>
 </template>
--- layouts/post.vue
+++ layouts/post.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="post-layout prose prose-zinc m-auto max-w-3xl px-1 py-10 dark:prose-invert sm:rounded sm:px-8 sm:pt-14 lg:relative">
+  <div
+    class="post-layout prose prose-zinc m-auto max-w-3xl px-1 py-10 dark:prose-invert
+    sm:(rounded px-8 pt-14) lg:relative"
+  >
     <slot />
   </div>
 </template>
@@ -8,6 +11,6 @@
 /* Customize headers to remove default underline */
 .prose h2 a,
 .prose h3 a {
-  @apply no-underline hover:border-b-solid hover:border-zinc-600 dark:hover:border-zinc-300;
+  @apply no-underline hover:(border-b-solid border-zinc-600) dark:hover:border-zinc-300;
 }
 </style>
