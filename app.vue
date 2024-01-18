93db97c8229e06ff7e111a06c990c18dce967096
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2024-01-18 15:31:16 +0700

    chore: check native loading indicator

--- app.vue
+++ app.vue
@@ -2,7 +2,8 @@
   <div class="app font-sans">
     <Html lang="en" />
     <Meta property="og:image" content="/cover.webp" />
-    <AppLoadingBar />
+    <!-- <AppLoadingBar /> -->
+    <NuxtLoadingIndicator color="red" />
     <AppNavbar />
     <NuxtLayout>
       <NuxtPage />
