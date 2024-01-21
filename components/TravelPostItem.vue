cedd77179a7904ed01aa49e5f2fde6a171d7ba96
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2024-01-21 16:22:20 +0700

    perf: size down imgs

--- components/TravelPostItem.vue
+++ components/TravelPostItem.vue
@@ -24,7 +24,7 @@ const { cleanString: cleanTitle, metaDesc, imgID, postDesc } = useCleanString(pr
       :src="props.post.imageUrl"
       class="my-0 h-52 w-full xs:rounded-lg object-cover"
       alt="blog post cover image"
-      width="800"
+      width="600"
       preload
       loading="eager"
       :placeholder="[50, 25, 75, 5]"
--- pages/travel/[...travel].vue
+++ pages/travel/[...travel].vue
@@ -37,7 +37,7 @@ const { cleanString: cleanTitle, metaDesc, imgID, postDesc } = useCleanString(cu
         alt="cover"
         class="object-cover h-full w-full"
         loading="eager"
-        width="800"
+        width="600"
       />
     </div>
     <div
