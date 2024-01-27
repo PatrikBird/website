a56c8263aa7724bca28672bd5ed4e3f55e565acd
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2024-01-27 14:51:56 +0700

    fix: a tag underline

---
+++ components/content/ProseA.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String,
+    default: undefined,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<style scoped lang="sass">
+a
+  border-bottom: 1.5px solid rgba(125, 125, 125, 0.7)
+  transition: border 0.2s ease-in-out
+  &:hover
+    border-bottom: 1.7px solid var(--fg)
+</style>
